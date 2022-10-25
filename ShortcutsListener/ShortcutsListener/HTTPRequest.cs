7c084aa1d13562aa67b2af2436a2bd88895e1a2f
bariseksi <bariseksi@gmail.com>
2022-10-25 15:08:36 +0300

    Add files

---
+++ ShortcutsListener/ShortcutsListener/HTTPRequest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortcutsListener
+{
+    public static class HTTPRequest
+    {
+        private static byte[] byteArray = new byte[10000];
+        public static Dictionary<string, string> Headers = new Dictionary<string, string>();
+        public static string Method;
+        public static string Request;
+        public static string BasicResponse = "HTTP/1.1 200 OK\n";
+
+        static public void Parse(NetworkStream stream)
+        {
+            int foundCounter = 0;
+            byte[] endOfLine = { 13, 10 };
+            bool parsingDone = false;
+            int numberOfLineFound = 0;
+            Headers.Clear();
+            while(!parsingDone)
+            {
+                for (int i = 0; i < byteArray.Length; i++)
+                {
+                    byte _byte = (byte)stream.ReadByte(); //reads -1 when reaches the end
+                    byteArray[i] = _byte;
+                    if(_byte == endOfLine[foundCounter])
+                    {
+                        foundCounter++;
+                        if(foundCounter == 2) //we found the end of line
+                        {
+                            if(i == 1)
+                            {
+                                parsingDone = true;
+                                break;
+                            }
+                            foundCounter = 0;
+                            numberOfLineFound++;
+                            if(numberOfLineFound == 1) //first line
+                            {
+                                string[] values = Encoding.ASCII.GetString(byteArray, 0, i - 1).Split(' ');
+                                Method = values[0].Trim('\r', ' ');
+                                Request = values[1].Trim('\r', ' ');
+                            }
+                            else
+                            {
+                                string[] values = Encoding.ASCII.GetString(byteArray, 0, i - 1).Split(':');
+                                Headers.Add(values[0].Trim('\r', ' ').ToLower(), values[1].Trim('\r', ' ').ToLower());
+                            }
+                            break; 
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    public static class HTTPReqHeaderKey
+    {
+        public const string ContentType = "content-type";
+        public const string FileName = "filename";
+        public const string ContentLength = "content-length";
+    }
+
+    public static class HTTPReqHeaderValue
+    {
+        public const string ContentType_Image_JPEG = "image/jpeg";
+        public const string ContentType_Image_HEIC = "image/heic";
+        public const string ContentType_Image_PNG = "image/png";
+        public const string ContentType_Text_Plain = "text/plain";
+        public const string ContentType_Video_MP4 = "video/mp4";
+        public const string ContentType_Video_MOV = "video/quicktime";
+        public const string ContentType_text_HTML = "text/html";
+    }
+}
---
+++ ShortcutsListener/ShortcutsListener/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShortcutsListener
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int port = 2560;
+            byte[] responseBytes = Encoding.ASCII.GetBytes(HTTPRequest.BasicResponse);
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            byte[] msg = new byte[10000];
+            int readCounter = 0;
+            int numberOfBytesRead = 0;
+
+            server.Start();
+
+            while (true)
+            {
+                Console.WriteLine($"Listening on port {port}");
+                TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+                NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+                HTTPRequest.Parse(ns);
+
+                //get the file size 
+                if (HTTPRequest.Headers.ContainsKey(HTTPReqHeaderKey.ContentLength) && int.TryParse(HTTPRequest.Headers[HTTPReqHeaderKey.ContentLength], out int numberOfbytesToRead))
+                {
+
+                    string fileName = null;
+                    if (HTTPRequest.Headers.ContainsKey(HTTPReqHeaderKey.FileName))
+                    {
+                        fileName = HTTPRequest.Headers[HTTPReqHeaderKey.FileName];
+                    }
+                    else //if file name is not specified generate something unique
+                    {
+                        fileName = $"file_{Guid.NewGuid()}";
+                    }
+
+                    //get the extention of the file its been always image/*, video/*, */*
+                    string fileExtention = HTTPRequest.Headers[HTTPReqHeaderKey.ContentType].Split('/')[1].ToLower();
+
+
+                    switch (fileExtention)
+                    {
+                        case "plain":
+                            fileExtention = "txt";
+                            break;
+                        case "quicktime": //ios puts video/quicktime content-type header for their video files.
+                            fileExtention = "mov";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    fileName = fileName + "." + fileExtention;
+
+                    Console.WriteLine(fileName);
+                    FileStream fs = new FileStream(fileName, FileMode.Create);
+                    readCounter = 0;
+                    while (readCounter < numberOfbytesToRead)
+                    {
+                        numberOfBytesRead = ns.Read(msg, 0, msg.Length);
+                        readCounter += numberOfBytesRead;
+                        fs.Write(msg, 0, numberOfBytesRead);
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        Console.Write($"{((UInt64)readCounter * 100) / ((UInt64)numberOfbytesToRead - 1)}% Completed");
+                    }
+                    fs.Close();
+                    Console.WriteLine('\n');
+                }
+                ns.Write(responseBytes, 0, responseBytes.Length);
+                ns.Close();
+                client.Close();
+            }
+        }
+    }
+}
